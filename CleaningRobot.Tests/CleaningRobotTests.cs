42248111eee6f0c7b4998d33d7028fd29676d323
Rene <rene.perez.castro@gmail.com>
2018-06-03 15:43:12 -0500

    Initial domain design and unit testing

---
+++ CleaningRobot.Tests/CleaningRobotTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using Should;
+using CleaningRobot.Domain;
+
+namespace CleaningRobot.Tests
+{
+    public class CleaningRobotTests
+    {
+		[Fact]
+		public void CleaningRobot_faces_west_when_turn_left_from_north()
+		{
+			var sut = CreateRobot(OrientationEnum.North);
+			sut.ExecuteInstruction(InstructionEnum.TurnLeft);
+			sut.CurrentPosition.ShouldEqual(new Position(0, 0));
+		}
+
+		[Fact]
+		public void CleaningRobot_faces_south_when_turn_left_from_west()
+		{
+			var sut = CreateRobot(OrientationEnum.West);
+			sut.ExecuteInstruction(InstructionEnum.TurnLeft);
+			sut.CurrentPosition.ShouldEqual(new Position(0, 0));
+		}
+
+		[Fact]
+		public void CleaningRobot_faces_east_when_turn_left_from_south()
+		{
+			var sut = CreateRobot(OrientationEnum.South);
+			sut.ExecuteInstruction(InstructionEnum.TurnLeft);
+			sut.CurrentPosition.ShouldEqual(new Position(0, 0));
+		}
+
+		[Fact]
+		public void CleaningRobot_faces_north_when_turn_left_from_east()
+		{
+			var sut = CreateRobot(OrientationEnum.East);
+			sut.ExecuteInstruction(InstructionEnum.TurnLeft);
+			sut.CurrentPosition.ShouldEqual(new Position(0, 0));
+		}
+
+		private Robot CreateRobot(OrientationEnum orientation)
+		{
+			return new Robot(100, new Position(0, 0), new Orientation(orientation));
+		}
+	}
+}
---
+++ CleaningRobot/Domain/CleaningRobot.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningRobot.Domain
+{
+	public class Robot
+	{
+		public int Battery { get; private set; }
+
+		public Position CurrentPosition { get; private set; }
+
+		public Orientation CurrentOrientation { get; private set; }
+
+		public Robot(int initialBattery, Position currentPosition, Orientation currentOrientation)
+		{
+			Battery = initialBattery;
+			CurrentPosition = currentPosition;
+			CurrentOrientation = currentOrientation;
+		}
+
+		public void ExecuteInstruction(InstructionEnum instruction)
+		{
+			
+		}
+	}
+
+	public enum InstructionEnum
+	{
+		TurnLeft,
+		TurnRight,
+		Advance,
+		Clean,
+		Back
+	}
+
+	public struct Position
+	{
+		public Position(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public int X { get; private set; }
+
+		public int Y { get; private set; }
+	}
+
+	public enum OrientationEnum
+	{
+		North,
+		South,
+		East,
+		West
+	}
+
+	public struct Orientation
+	{
+		public OrientationEnum CurrentOrientation { get; private set; }
+
+		public Orientation(OrientationEnum currentOrientation)
+		{
+			CurrentOrientation = currentOrientation;
+		}
+
+		public Orientation TurnLeft()
+		{
+			switch (CurrentOrientation)
+			{
+				case OrientationEnum.North:
+					return new Orientation(OrientationEnum.West);
+				case OrientationEnum.West:
+					return new Orientation(OrientationEnum.South);
+				case OrientationEnum.South:
+					return new Orientation(OrientationEnum.East);
+				case OrientationEnum.East:
+					return new Orientation(OrientationEnum.North);
+				default:
+					throw new InvalidOperationException();
+			}
+		}
+
+	}
+
+}
